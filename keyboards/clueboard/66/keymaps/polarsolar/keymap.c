7a10f0c531625a1aeb218d62e2db8406d6e27f1a
polarsolar <40618057+polarsolar@users.noreply.github.com>
2018-09-08 21:05:45 -0700

    Zlant keymap compiles, but does it work?

--- keyboards/clueboard/66/keymaps/polarsolar/keymap.c
+++ keyboards/clueboard/66/keymaps/polarsolar/keymap.c
@@ -23,7 +23,6 @@ extern keymap_config_t keymap_config;
 enum {
   TD_U_H = 0,
   TD_D_E,
-  TD_4,
   TD_N
 };
 
@@ -32,7 +31,6 @@ qk_tap_dance_action_t tap_dance_actions[] = {
   // Tap once for Page Up or Page Down; tap twice for Home or End
   [TD_U_H] = ACTION_TAP_DANCE_DOUBLE(KC_PGUP, KC_HOME),
   [TD_D_E] = ACTION_TAP_DANCE_DOUBLE(KC_PGDN, KC_END),
-  [TD_4] = ACTION_TAP_DANCE_DOUBLE(KC_4, KC_F4),
   [TD_N] = ACTION_TAP_DANCE_DOUBLE(KC_GRV, KC_NLCK)
 };
 
@@ -42,6 +40,7 @@ qk_tap_dance_action_t tap_dance_actions[] = {
 #define FN_CAPS LT(_FL, KC_CAPS)            // Tap for Caps Lock, Hold for Function Layer
 #define OOPS GUI_T(KC_NO)                   // Nothing on single press, GUI on hold
 #define CTL_ALT MT(MOD_LCTL | MOD_LALT, KC_NO)
+#define ALT_MODS (MOD_BIT(KC_LALT) | MOD_BIT(KC_RALT))
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   /* Keymap _DVORAK: Base layer (default layer), but other layers can also be reached from the Qwerty layer.
@@ -60,10 +59,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    */
 
 [_DVORAK] = LAYOUT (
-  KC_GESC, KC_1,    KC_2,    KC_3,    TD(TD_4),KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_LBRC, KC_RBRC, KC_BSPC, KC_DEL,           TD(TD_U_H), 
-  KC_TAB,  KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,    KC_F,    KC_G,    KC_C,    KC_R,    KC_L,    KC_SLSH, KC_EQL,  KC_BSLS,                   TD(TD_D_E), 
-  FN_CAPS, KC_A,    KC_O,    KC_E,    KC_U,    KC_I,    KC_D,    KC_H,    KC_T,    KC_N,    KC_S,    KC_MINS, XXXXXXX, KC_ENT,                             
-  KC_LSFT, XXXXXXX, KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,    KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,    KC_RSFT, TD(TD_N),         KC_UP,            
+  KC_GESC, KC_1,    KC_2,    KC_3,    F(1),    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_LBRC, KC_RBRC, KC_BSPC, KC_DEL,           TD(TD_U_H),
+  KC_TAB,  KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,    KC_F,    KC_G,    KC_C,    KC_R,    KC_L,    KC_SLSH, KC_EQL,  KC_BSLS,                   TD(TD_D_E),
+  FN_CAPS, KC_A,    KC_O,    KC_E,    KC_U,    KC_I,    KC_D,    KC_H,    KC_T,    KC_N,    KC_S,    KC_MINS, XXXXXXX, KC_ENT,
+  KC_LSFT, XXXXXXX, KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,    KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,    KC_RSFT, TD(TD_N),         KC_UP,
   KC_LCTL, CTL_ALT, OOPS,    KC_LALT,                   KC_BSPC, KC_SPC,                    TT(1),   TT(2),   MO(3),   KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT),
 
   /* Keymap _QWERTY: Qwerty layer.  Layer indicator color: green.
@@ -80,10 +79,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `-----------------------------------------------------------------------------------------------------------------------------------'
   */
 [_QWERTY] = LAYOUT (
-  KC_GESC, KC_1,    KC_2,    KC_3,    TD(TD_4),KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_DEL,           TD(TD_U_H), 
-  KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,                   TD(TD_D_E), 
-  FN_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, XXXXXXX, KC_ENT,                             
-  KC_LSFT, XXXXXXX, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, TD(TD_N),         KC_UP,            
+  KC_GESC, KC_1,    KC_2,    KC_3,    F(1),    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_DEL,           TD(TD_U_H),
+  KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,                   TD(TD_D_E),
+  FN_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, XXXXXXX, KC_ENT,
+  KC_LSFT, XXXXXXX, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, TD(TD_N),         KC_UP,
   KC_LCTL, CTL_ALT, OOPS,    KC_LALT,                   KC_BSPC, KC_SPC,                    TT(1),   TT(2),   MO(3),   KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT),
 
   /* Keymap _NM: Numpad and mouse layer, but the mouse isn't active yet (and isn't enabled yet).  Layer indicator color: yellow-ish.
@@ -100,28 +99,28 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `-----------------------------------------------------------------------------------------------------------------------------------'
    */
 [_NM] = LAYOUT (
-  KC_GESC, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_PSLS, _______, _______, KC_BSPC, KC_DEL,           TD(TD_U_H), 
-  _______, _______, _______, _______, _______, _______, _______, KC_P7,   KC_P8,   KC_P9,   KC_PAST, _______, _______, _______,                   TD(TD_D_E), 
-  _______, _______, _______, _______, _______, _______, _______, KC_P4,   KC_P5,   KC_P6,   KC_PMNS, _______, _______, _______,                            
-  _______, _______, _______, _______, _______, _______, _______, _______, KC_P1,   KC_P2,   KC_P3,   KC_PPLS, _______, _______,          KC_UP,          
+  KC_GESC, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_PSLS, _______, _______, KC_BSPC, KC_DEL,           TD(TD_U_H),
+  _______, _______, _______, _______, _______, _______, _______, KC_P7,   KC_P8,   KC_P9,   KC_PAST, _______, _______, _______,                   TD(TD_D_E),
+  _______, _______, _______, _______, _______, _______, _______, KC_P4,   KC_P5,   KC_P6,   KC_PMNS, _______, _______, _______,
+  _______, _______, _______, _______, _______, _______, _______, _______, KC_P1,   KC_P2,   KC_P3,   KC_PPLS, _______, _______,          KC_UP,
   _______, _______, _______, _______,                   KC_BSPC, KC_P0,                     KC_PDOT, TT(2),   MO(3),   _______, KC_LEFT, KC_DOWN, KC_RGHT),
 
   /* Keymap _FL: Function Layer.  Layer indicator color: blue.
    */
 [_FL] = LAYOUT (
-  TG(_FL),  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, _______, KC_DEL,           BL_STEP, 
-  _______, _______, _______, _______, _______, _______, _______, _______, KC_PSCR, KC_SLCK, KC_PAUS, _______, _______, _______,                   _______, 
-  _______, _______, MO(_CL), _______, _______, _______, _______, KC_MUTE, KC_VOLD, KC_VOLU, _______, _______, _______, _______,                            
-  _______, _______, _______, _______, _______, _______, _______, KC_MPRV, KC_MPLY, KC_MNXT, _______, _______, _______, _______,          KC_PGUP,          
+  TG(_FL),  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, _______, KC_DEL,           BL_STEP,
+  _______, _______, _______, _______, _______, _______, _______, _______, KC_PSCR, KC_SLCK, KC_PAUS, _______, _______, _______,                   _______,
+  _______, _______, MO(_CL), _______, _______, _______, _______, KC_MUTE, KC_VOLD, KC_VOLU, _______, _______, _______, _______,
+  _______, _______, _______, _______, _______, _______, _______, KC_MPRV, KC_MPLY, KC_MNXT, _______, _______, _______, _______,          KC_PGUP,
   _______, _______, _______, _______,                   _______, _______,                   _______, _______, _______, _______, KC_HOME, KC_PGDN, KC_END),
-  
+
   /* Keymap _CL: Control layer.  Layer indicator color: red.
    */
 [_CL] = LAYOUT (
-  TG(_CL), _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, RGB_TOG,          RGB_VAI, 
-  _______, _______, _______, _______, RESET,   _______, _______, _______, _______, _______, _______, _______, _______, RGB_RST,                   RGB_VAD, 
-  _______, _______, MO(_CL), _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,                            
-  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          RGB_SAI,          
+  TG(_CL), _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, RGB_TOG,          RGB_VAI,
+  _______, _______, _______, _______, RESET,   _______, _______, _______, _______, _______, _______, _______, _______, RGB_RST,                   RGB_VAD,
+  _______, _______, MO(_CL), _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          RGB_SAI,
   _______, _______, _______, _______,                   RGB_MOD, RGB_MOD,                   _______, _______, _______, _______, RGB_HUD, RGB_SAD, RGB_HUI),
 
 
@@ -133,15 +132,38 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 const uint16_t PROGMEM fn_actions[] = {
   [0] = ACTION_FUNCTION(0),  // Calls action_function()
+  [1] = ACTION_FUNCTION(1)
 };
 
 void action_function(keyrecord_t *record, uint8_t id, uint8_t opt) {
+  static uint8_t mods_pressed;
+
   switch (id) {
     case 0:
       if (record->event.pressed) {
         rgblight_mode(1);
         rgblight_sethsv(320, 255, 255);
       }
+      case 1:
+        mods_pressed = get_mods()&ALT_MODS;
+        if (record->event.pressed) {
+          if(mods_pressed) {
+            add_key(KC_F4);
+            send_keyboard_report();
+          } else {
+            add_key(KC_4);
+            send_keyboard_report();
+          }
+        } else {
+          if (mods_pressed) {
+            del_key(KC_F4);
+            send_keyboard_report();
+          } else {
+            del_key(KC_4);
+            send_keyboard_report();
+        }
+      }
+      break;
   }
 }
 
--- keyboards/zlant/keymaps/polarsolar/keymap.c
+++ keyboards/zlant/keymaps/polarsolar/keymap.c
@@ -1,6 +1,5 @@
 // 40%: polarsolar cannot mannage.
-// Still a major work in progress. No, it won't compile & I know it.
-// Keymap also requires some (also-work-in-progress) files from a local user directory.
+// Still a major work in progress.  It compiles, but I don't have the PCB yet to actually test it out.
 
 #include "zlant.h"
 #include "action_layer.h"
@@ -25,19 +24,23 @@ extern keymap_config_t keymap_config;
 
 // Tap Dance Declarations
 enum {
-  TD_U_H = 0,
-  TD_D_E,
-  TD_4,
-  TD_N
+  TD_L = 0,
+  TD_D,
+  TD_U,
+  TD_R,
+  TD_N,
+  TD_W
 };
 
 // Tap Dance Definitions
 qk_tap_dance_action_t tap_dance_actions[] = {
   // Tap once for Page Up or Page Down; tap twice for Home or End
-  [TD_U_H] = ACTION_TAP_DANCE_DOUBLE(KC_PGUP, KC_HOME),
-  [TD_D_E] = ACTION_TAP_DANCE_DOUBLE(KC_PGDN, KC_END),
-  [TD_4] = ACTION_TAP_DANCE_DOUBLE(KC_4, KC_F4),
-  [TD_N] = ACTION_TAP_DANCE_DOUBLE(KC_GRV, KC_NLCK)
+  [TD_L] = ACTION_TAP_DANCE_DOUBLE(KC_LEFT, KC_HOME),
+  [TD_D] = ACTION_TAP_DANCE_DOUBLE(KC_DOWN, KC_PGDN),
+  [TD_U] = ACTION_TAP_DANCE_DOUBLE(KC_UP, KC_PGUP),
+  [TD_R] = ACTION_TAP_DANCE_DOUBLE(KC_RGHT, KC_END),
+  [TD_N] = ACTION_TAP_DANCE_DOUBLE(KC_GRV, KC_NLCK),
+  [TD_W] = ACTION_TAP_DANCE_DOUBLE(KC_LBRC, KC_RBRC)
 };
 
 // Useful defines
@@ -47,6 +50,7 @@ qk_tap_dance_action_t tap_dance_actions[] = {
 #define OOPS GUI_T(KC_NO)                   // Nothing on single press, GUI on hold
 #define CTL_ALT MT(MOD_LCTL | MOD_LALT, KC_NO)
 #define CENTER LT(4, KC_DEL)
+#define ALT_MODS (MOD_BIT(KC_LALT) | MOD_BIT(KC_RALT))
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   /* Keymap _DVORAK: Base layer (default layer), but other layers can also be reached from the Qwerty layer.
@@ -55,17 +59,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     * |------+------+------+------+------+-------------+------+------+------+------+------|
      * |LO_ESC|   A  |   O  |   E  |   U  |   I  |   D  |   H  |   T  |   N  |   S  |Ent++!|  //Tap for Esc; hold for MO(_LOWER) //Tap for Enter; hold for ???
       * |------+------+------+------+------+------|------+------+------+------+------+------|
-       * |ShiftR|   ;  |   Q  |   J  |   K  |   X  |   B  |   M  |   W  |   V  |   Z  |ShiftU|  //Tap=Shift; double-tap=RAISE //Tap=up; single hold=Shift; double-tap=RAISE
+       * |ShiftR|   ;  |   Q  |   J  |   K  |   X  |   B  |   M  |   W  |   V  |   Z  |ShiftU|  //Tap=Shift; double-tap=RAISE
         * |------+------+------+------+------+------+------+------+------+------+------+------|
-         * | Ctrl |CTLALT| OOPS |  Alt | Bksp |CENTER|  Spc |TT(1) |      | Left | Down |Right |  //Tap for del; hold for MO(_FL) //Tap-toggle Qwerty
+         * | Ctrl |CTLALT| OOPS |  Alt | Bksp |CENTER|  Spc |TT(1) | TD_L | TD_D | TD_U | TD_R |  //Tap for del; hold for MO(_FL) //Tap-toggle Qwerty
          * `-----------------------------------------------------------------------------------'
    */
 
 [_DVORAK] = LAYOUT (
-  KC_TAB,  KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,    KC_F,    KC_G,    KC_C,    KC_R,    KC_L,    KC_SLSH,
+  KC_TAB,  KC_QUOT, KC_COMM, KC_DOT,  F(1),    KC_Y,    KC_F,    KC_G,    KC_C,    KC_R,    KC_L,    KC_SLSH,
   LO_ESC,  KC_A,    KC_O,    KC_E,    KC_U,    KC_I,    KC_D,    KC_H,    KC_T,    KC_N,    KC_S,    KC_ENT,
   KC_LSFT, KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,    KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,    KC_RSFT,
-  KC_LCTL, CTL_ALT, OOPS,    KC_LALT, KC_BSPC, CENTER,  KC_SPC,  TT(1),   _______, KC_UP,   KC_DOWN, KC_RGHT),
+  KC_LCTL, CTL_ALT, OOPS,    KC_LALT, KC_BSPC, CENTER,  KC_SPC,  TT(1),   TD(TD_L),TD(TD_D),TD(TD_U),TD(TD_R)),
 
   /* Keymap _QWERTY: Qwerty layer. Straight map back from Dvorak. Only for use as fake-Dvorak on computers that have Dvorak enabled on the OS.
    * ,-----------------------------------------------------------------------------------.
@@ -73,16 +77,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     * |------+------+------+------+------+-------------+------+------+------+------+------|
      * |LO_ESC|   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |Ent++!|  //Tap for Esc; hold for MO(_LOWER) //Tap for Enter; hold for ???
       * |------+------+------+------+------+------+------+------+------+------+------+------|
-       * |ShiftR|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |ShiftU|  //Tap or single hold = Shift; double-tap=RAISE //Tap=up; single hold=Shift; double-tap=RAISE;
+       * |ShiftR|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |ShiftU|  //Tap or single hold = Shift; double-tap=RAISE
         * |------+------+------+------+------+------+------+------+------+------+------+------|
-         * | Ctrl |CTLALT| OOPS | Alt  | Bksp |CENTER|  Spc |TT(1) |      | Left | Down |Right |  //Tap for del; hold for MO(FL) //Tap-toggle Qwerty
+         * | Ctrl |CTLALT| OOPS | Alt  | Bksp |CENTER|  Spc |TT(1) | TD_L | TD_D | TD_U | TD_R |  //Tap for del; hold for MO(FL) //Tap-toggle Qwerty
          * `-----------------------------------------------------------------------------------'
    */
 [_QWERTY] = LAYOUT (
-  KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,
+  KC_TAB,  KC_Q,    KC_W,    KC_E,    F(2),    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,
   LO_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-  KC_LCTL, CTL_ALT, OOPS,    KC_LALT, KC_BSPC, CENTER,  KC_SPC,  TT(1),   _______, KC_LEFT, KC_DOWN, KC_RGHT),
+  KC_LCTL, CTL_ALT, OOPS,    KC_LALT, KC_BSPC, CENTER,  KC_SPC,  TT(1),   TD(TD_L),TD(TD_D),TD(TD_U),TD(TD_R)),
 
   /* Layer _RAISE
    * ,-----------------------------------------------------------------------------------.
@@ -119,39 +123,40 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   /* Layer _FL
    * ,-----------------------------------------------------------------------------------.
-   * |  `   |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  |[] {} |  //Tap for [{; double tap for ]}
+   * |  `   |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | TD_W |  //Tap for [{; double tap for ]}
     * |------+------+------+------+------+-------------+------+------+------+------+------|
-     * |      |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |      |      |      |      |  -   |  //
+     * | TD_N |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |      |      |      |      |  -   |  //
       * |------+------+------+------+------+------|------+------+------+------+------+------|
-       * |Shift |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |      |      |      |      | PgUp |  //Tap for PgUp; double-tap for Home
+       * |Shift |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |      |      |      |      |      |
         * |------+------+------+------+------+------+------+------+------+------+------+------|
-         * |      |      |      |      | Bksp |  Del |  Spc |      |      |      | PgDn |      |  //Tap for PgDn; double-tap for End
-         * `-----------------------------------------------------------------------------------'   */
+         * |      |      |      |      | Bksp |  Del |  Spc |      |      |      |      |      |
+         * `-----------------------------------------------------------------------------------'
+    */
   /* Keymap _FL: Function Layer.
    */
 [_FL] = LAYOUT (
-  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSLS,
-  _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   _______, _______, _______, _______, KC_MINS,
-  KC_LSFT, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______, _______, _______, TD(TD_U_H),
-  _______, _______, _______, _______, KC_BSPC, CENTER,  KC_SPC,  _______, _______, _______, TD(TD_D_E),_______),
+  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    TD(TD_W),
+  TD(TD_N),KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   _______, _______, _______, _______, KC_MINS,
+  KC_LSFT, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______, _______, _______, _______,
+  _______, _______, _______, _______, KC_BSPC, _______, KC_SPC,  _______, _______, _______, _______, _______),
 
   /* Keymap _CL: Control layer.  Layer indicator color: red.
    * ,-----------------------------------------------------------------------------------.
    * |TG(_CL)|      |      |      |RESET |      |      |      |      |      |      |BLtog | //BL_TOGG
     * |------+------+------+------+------+------+------+------+------+------+------+------|
-     * |      |      |      |      |      |      |      |      |      |      |      | F[0] |
+     * |      |      |      |      |      |      |      |      |      |      |      | F(0) |
       * |------+------+------+------+------+------+------+------+------+------+------+------|
-       * |      |      |      |      |      |      |      |      |      |      |      |BL_INC|
+       * |      |      |      |      |      |      |      |      |      |      |      |      |
         * |------+------+------+------+------+------+------+------+------+------+------+------|
-         * |      |      |      |      |      |      |      |      |      |      |BL_DEC|      |
+         * |      |      |      |      |      |      |      |      | HUD  | SAD  | SAI  | HUI |
          * `-----------------------------------------------------------------------------------'
 
    */
 [_CL] = LAYOUT (
   TG(_CL), _______, _______, _______, RESET,   _______, _______, _______, _______, _______, _______, RGB_TOG,
   _______, _______, MO(_CL), _______, _______, _______, _______, _______, _______, _______, _______, RGB_RST,
-  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, RGB_SAI,
-  _______, _______, _______, _______, _______, _______, KC_SPC,  _______, _______, RGB_HUD, RGB_SAD, RGB_HUI),
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+  _______, _______, _______, _______, _______, _______, KC_SPC,  _______, RGB_HUD, RGB_SAD, RGB_SAI, RGB_HUI),
 
 };
 
@@ -161,15 +166,57 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 const uint16_t PROGMEM fn_actions[] = {
   [0] = ACTION_FUNCTION(0),  // Calls action_function()
+  [1] = ACTION_FUNCTION(1),
+  [2] = ACTION_FUNCTION(2),
 };
 
 void action_function(keyrecord_t *record, uint8_t id, uint8_t opt) {
+  static uint8_t mods_pressed;
+  mods_pressed = get_mods()&ALT_MODS;
+
   switch (id) {
     case 0:
       if (record->event.pressed) {
         rgblight_mode(1);
         rgblight_sethsv(320, 255, 255);
       }
+    case 1:
+      if (record->event.pressed) {
+        if (mods_pressed) {
+          add_key(KC_F4);
+          send_keyboard_report();
+        } else {
+          add_key(KC_P);
+          send_keyboard_report();
+        }
+      } else {
+        if(mods_pressed) {
+          del_key(KC_F4);
+          send_keyboard_report();
+        } else {
+          del_key(KC_P);
+          send_keyboard_report();
+        }
+      }
+    case 2:
+      if (record->event.pressed) {
+        if (mods_pressed) {
+          add_key(KC_F4);
+          send_keyboard_report();
+        } else {
+          add_key(KC_R);
+          send_keyboard_report();
+        }
+      } else {
+        if(mods_pressed) {
+          del_key(KC_F4);
+          send_keyboard_report();
+        } else {
+          del_key(KC_R);
+          send_keyboard_report();
+        }
+      }
+    break;
   }
 }
 
